f9dbee58c30d5e19686db4c1d3b05421f01ea7e9
APP技術部-陳冠志 <bruce@mitake.com.tw>
2020-07-08 14:24:27 +0800

    Initial Commit

---
+++ TestFirebaseCrashlytics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestFirebaseCrashlytics
+//
+//  Created by APP技術部-陳冠志 on 2020/7/8.
+//  Copyright © 2020 Bruce Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
